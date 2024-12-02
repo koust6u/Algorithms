d877c9e6bf017e8a42879f2ffeef30c25f1d3246
김민종 <koust6u@pusan.ac.kr>
2024-12-02 21:19:52 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    // 면목이 없구만요
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        final String name = scanner.nextLine();
+
+        System.out.println(name + "??!");
+    }
+}
