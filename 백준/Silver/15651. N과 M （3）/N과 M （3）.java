a81ba5c5e8c013f2c16a5bb012e58835ff3892cc
김민종 <koust6u@pusan.ac.kr>
2025-02-09 22:06:24 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 67888 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(s.nextToken());
+        int M = Integer.parseInt(s.nextToken());
+
+        arr = new int[M];
+        backTracking(N, M, 0);
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int N, int M, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            backTracking(N, M, depth + 1);
+        }
+    }
+}
