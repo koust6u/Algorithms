284a03aef507579a64bd6804396aee21ed230c3b
김민종 <koust6u@pusan.ac.kr>
2025-02-10 22:15:56 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        String cat = """
+            \\    /\\
+             )  ( ')
+            (  /  )
+             \\(__)|
+            """;
+
+        System.out.println(cat);
+    }
+}
